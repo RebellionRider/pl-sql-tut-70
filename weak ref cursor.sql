18ec1fa49784645e8691fe099ebdc2c5d873ffdb
Manish Sharma <32567588+RebellionRider@users.noreply.github.com>
2017-10-17 16:08:07 +0530

    Update weak ref cursor.sql

--- weak ref cursor.sql
+++ weak ref cursor.sql
@@ -1,7 +1,17 @@
+/*
+    Refer PL/SQL Tutorial 70 for detail knowledge of Weak Ref Cursor. Here are the Links
+    Video:https://youtu.be/_u7xkaRzai8
+    Blog: http://bit.ly/ref-cursor-3
+    Subscribe to the YouTube channel for more tutorials
+    www.YouTube.com/RebellionRider
+*/
 SET SERVEROUTPUT ON;
 DECLARE
+    /*Declare Weak Ref Cursor*/
     TYPE wk_RefCur IS REF CURSOR;
+    /*Declare Cursor Variable of ref cursor type*/
     cur_var wk_RefCur;
+    /*Declare two "Anchored Datatype Variable" for holding data from the cursor*/
     f_name  employees.first_name%TYPE;
     emp_sal employees.salary%TYPE;
 BEGIN
@@ -11,3 +21,12 @@ BEGIN
        DBMS_OUTPUT.PUT_LINE(f_name||' '||emp_sal);
 END;
 /
+
+
+/*
+    Social Media Links
+    Follow me on Social Media for regular updates
+    www.Facebook.com/TheRebellionRider
+    www.Twitter.com/RebellionRider
+    www.Instagram.com/RebellionRider
+*/
